6fa00e5971c3db3fd64813d8c6abbb2758b7fc37
Galen Kim Davis <galen@kube1.zhenglim.fun>
2021-01-28 17:03:19 +0000

    Committer: Galen Kim Davis <galen@kube1.zhenglim.fun>  Changes to be committed: 	new file:   temp_log.sh

---
+++ temp_log.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script: temp_log.sh
+# Purpose: Log temperature of hw to /var/log/temperature.log
+# Author: Galen Kim Davis <galen@redkibble.com> under GPL v2.x+
+# ---------------------------------------------------------------
+
+timestamp() {
+  date +%Y%m%d%H%M%S%Z
+}
+cpu=`cat /sys/devices/virtual/thermal/thermal_zone0/temp | sed 's/^\(.\{2\}\)/\1./'`
+gpu=`cat /sys/devices/virtual/thermal/thermal_zone1/temp | sed 's/^\(.\{2\}\)/\1./'`
+echo "$(timestamp) @ $(hostname) CPU: $cpu" >> /var/log/temperature.log
+echo "$(timestamp) @ $(hostname) GPU: $gpu" >> /var/log/temperature.log
